09dfbdcc290d90111afafa0402ad5eedf13882fd
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-11 11:38:35 -0300

    throw

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -1,12 +1,12 @@
 public class Fluxo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("Ini do main");
         metodo1();
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws Exception{
         System.out.println("Ini do metodo1");
         try {
             metodo2();
@@ -17,9 +17,9 @@ public class Fluxo {
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws Exception {
         System.out.println("Ini do metodo2");
-        throw new MinhaExcecao("deu errado");
+         new MinhaExcecao("deu errado");
 
     }
 }
